ce2562cda85da8d4c1209601bf50a2703550cc61
Lynx-Sumit <skumarupadhyay1@gmail.com>
2023-06-04 15:33:54 +0530

    Find the average of all elements in an array

--- Traversing/Q7.jsx
+++ Traversing/Q7.jsx
@@ -3,3 +3,11 @@
 // Input: [1, 2, 3, 4, 5]
 // Output: 3
 
+let arr = [1,2,3,4,5]
+let avg,sum=0,count=0
+for(let i=0;i<arr.length;i++){
+    sum+=arr[i]
+    count++
+}
+avg = sum/count
+console.log(avg)
