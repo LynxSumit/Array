4c3a5414e5bb26c2774f07daefaf49ca1d968228
Lynx-Sumit <skumarupadhyay1@gmail.com>
2023-06-05 09:14:06 +0530

    done with some changes in 10th question

--- Traversing/Q10.jsx
+++ Traversing/Q10.jsx
@@ -3,9 +3,13 @@
 // Input: [1, 2, 3, 4, 5]
 // Output: true
 
-let arr = [-11,21,31,41,-1]
-let result
-for(let i=0;i<arr.length-1;i++){
-    result = arr[i]<arr[i+1] && arr[i]>arr[i-1]
+let arr = [-245,921,1031,1031,4100,100000]
+let result = true
+
+for(let i=1;i<arr.length-1;i++){
+    if(!(arr[i]<=arr[i+1])){
+        result = false
+        break
+    }
 }
 console.log(result)
