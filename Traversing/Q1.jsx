0ee301344f657099c72389664bb0a25575288b89
Sumit Upadhyay <114245865+LynxSumit@users.noreply.github.com>
2023-06-10 14:18:21 +0530

    Delete Traversing directory

--- Traversing/Q1.jsx
+++
@@ -1,9 +0,0 @@
-// Name - Sumit Upadhyay
-// Print all elements of an array:
-// Input: [1, 2, 3, 4, 5]
-// Output: 1 2 3 4 5
-
-let arr1 = [1,2,3,4,5,6,7,3425,24,56,44,15]
-for(let i=0;i<arr1.length;i++){
-    console.log(arr1[i])
-}
